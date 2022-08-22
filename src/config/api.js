19bc66aa7a7b48f212c9df71d3efd85508a94952
George Chalikiopoulos <chalikiopoulosgr@gmail.com>
2022-08-22 05:30:22 +0300

    Add API config holding the required variables

---
+++ src/config/api.js
@@ -0,0 +1,16 @@
+/**
+
+Static API config for the sake of simplicity
+
+**/
+const API = {
+  URL: 'https://api-gateway-dev.phorest.com/third-party-api-server/api/business/',
+  AUTH: {
+    username: 'global/cloud@apiexamples.com',
+    password: 'VMlRo/eh+Xd8M~l'
+  },
+  BUSINESS_ID: 'eTC3QY5W3p_HmGHezKfxJw',
+  BRANCH_ID: 'SE-J0emUgQnya14mOGdQSw'
+};
+
+export default API;
